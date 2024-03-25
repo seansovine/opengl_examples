6292d1002a5fbc803c6a729288bfa0c64ba68df7
Sean Sovine <sean.r.sovine@gmail.com>
2024-03-24 20:49:03 -0400

    Some minor formatting

--- examples/de_Vries/hello_triangle/hello_triangle.cpp
+++ examples/de_Vries/hello_triangle/hello_triangle.cpp
@@ -17,19 +17,23 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}\0";
+const char *vertexShaderSource = R"TST(
+  #version 330 core
+  layout (location = 0) in vec3 aPos;
+  void main()
+  {
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+  }
+)TST";
 
-const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                   "}\n\0";
+const char *fragmentShaderSource = R"TST(
+  #version 330 core
+  out vec4 FragColor;
+  void main()
+  {
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+  }
+)TST";
 
 int main() {
   // glfw: initialize and configure
@@ -63,6 +67,7 @@ int main() {
 
   // build and compile our shader program
   // ------------------------------------
+
   // vertex shader
   unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -75,6 +80,7 @@ int main() {
     glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
     std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
+
   // fragment shader
   unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -85,11 +91,13 @@ int main() {
     glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
     std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
+
   // link shaders
   unsigned int shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
   glLinkProgram(shaderProgram);
+
   // check for linking errors
   glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
   if (!success) {
@@ -107,11 +115,13 @@ int main() {
       -0.5f, -0.5f, 0.0f, // bottom left
       -0.5f, 0.5f,  0.0f  // top left
   };
+
   unsigned int indices[] = {
       // note that we start from 0!
       0, 1, 3, // first Triangle
       1, 2, 3  // second Triangle
   };
+
   unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
