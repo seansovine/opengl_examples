1c80066981a4072ba5edb79c2ccd11b816317c73
Sean Sovine <sean.r.sovine@gmail.com>
2024-04-24 07:32:15 -0400

    Move some setup code to a separate function

--- examples/de_Vries/hello_triangle/hello_triangle.cpp
+++ examples/de_Vries/hello_triangle/hello_triangle.cpp
@@ -14,8 +14,16 @@
 
 #include <iostream>
 
+struct GlState {
+  GLuint vao;
+  GLuint vbo;
+  GLuint ebo;
+};
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+
+GlState setupVerticxState();
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -56,7 +64,6 @@ int main() {
 
   // vertex shader
   GLuint vertexShader = makeShader(vertexShaderSource, GL_VERTEX_SHADER);
-
   if (auto error = checkShaderCompile(vertexShader); error) {
     std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << error.value() << std::endl;
     glfwTerminate();
@@ -65,7 +72,6 @@ int main() {
 
   // fragment shader one
   GLuint fragmentShaderOne = makeShader(fragmentShaderSourceOne, GL_FRAGMENT_SHADER);
-
   if (auto error = checkShaderCompile(fragmentShaderOne); error) {
     std::cout << "ERROR::SHADER::FRAGMENT_ONE::COMPILATION_FAILED\n" << error.value() << std::endl;
     glfwTerminate();
@@ -74,7 +80,6 @@ int main() {
 
   // fragment shader one
   GLuint fragmentShaderTwo = makeShader(fragmentShaderSourceTwo, GL_FRAGMENT_SHADER);
-
   if (auto error = checkShaderCompile(fragmentShaderTwo); error) {
     std::cout << "ERROR::SHADER::FRAGMENT_TWO::COMPILATION_FAILED\n" << error.value() << std::endl;
     glfwTerminate();
@@ -83,7 +88,6 @@ int main() {
 
   // link shaders
   GLuint shaderProgramOne = makeProgram({vertexShader, fragmentShaderOne});
-
   if (auto error = checkProgramLink(shaderProgramOne); error) {
     std::cout << "ERROR::SHADER::PROGRAM_TWO::LINKING_FAILED\n" << error.value() << std::endl;
     glfwTerminate();
@@ -92,7 +96,6 @@ int main() {
 
   // link shaders
   GLuint shaderProgramTwo = makeProgram({vertexShader, fragmentShaderTwo});
-
   if (auto error = checkProgramLink(shaderProgramTwo); error) {
     std::cout << "ERROR::SHADER::PROGRAM_TWO::LINKING_FAILED\n" << error.value() << std::endl;
     glfwTerminate();
@@ -104,49 +107,7 @@ int main() {
   glDeleteShader(fragmentShaderTwo);
 
   // set up vertex data (and buffer(s)) and configure vertex attributes
-  // ------------------------------------------------------------------
-
-  float vertices[] = {
-      0.55f, 0.5f, 0.0f,   // top right
-      0.55f, -0.5f, 0.0f,  // bottom right
-      -0.45f, -0.5f, 0.0f, // bottom left
-      //
-      -0.55f, 0.5f, 0.0f,  // top left
-      -0.55f, -0.5f, 0.0f, // bottom left
-      0.45f, 0.5f, 0.0f    // top right
-  };
-
-  unsigned int indices[] = {
-      // note that we start from 0!
-      0, 1, 2, // first Triangle
-      3, 5, 4  // second Triangle
-  };
-
-  unsigned int VBO, VAO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
-
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s),
-  // and then configure vertex attributes(s).
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO as
-  // the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object
-  // IS stored in the VAO; keep the EBO bound.
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  GlState state = setupVerticxState();
 
   // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely
   // happens. Modifying other VAOs requires a call to glBindVertexArray anyways so we generally don't unbind
@@ -168,8 +129,8 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but
-                            // we'll do so to keep things a bit more organized
+    glBindVertexArray(state.vao); // seeing as we only have a single VAO there's no need to bind it every
+                                  // time, but we'll do so to keep things a bit more organized
 
     // draw our first triangle
     glUseProgram(shaderProgramOne);
@@ -194,10 +155,11 @@ int main() {
 
   // optional: de-allocate all resources once they've outlived their purpose:
   // ------------------------------------------------------------------------
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
+  glDeleteVertexArrays(1, &state.vao);
+  glDeleteBuffers(1, &state.vbo);
+  glDeleteBuffers(1, &state.ebo);
   glDeleteProgram(shaderProgramOne);
+  glDeleteProgram(shaderProgramTwo);
 
   // glfw: terminate, clearing all previously allocated GLFW resources.
   // ------------------------------------------------------------------
@@ -218,4 +180,51 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   // make sure the viewport matches the new window dimensions; note that width and
   // height will be significantly larger than specified on retina displays.
   glViewport(0, 0, width, height);
+}
+
+GlState setupVerticxState() {
+  GlState state;
+
+  float vertices[] = {
+      0.55f, 0.5f, 0.0f,   // top right
+      0.55f, -0.5f, 0.0f,  // bottom right
+      -0.45f, -0.5f, 0.0f, // bottom left
+      //
+      -0.55f, 0.5f, 0.0f,  // top left
+      -0.55f, -0.5f, 0.0f, // bottom left
+      0.45f, 0.5f, 0.0f    // top right
+  };
+
+  unsigned int indices[] = {
+      // note that we start from 0!
+      0, 1, 2, // first Triangle
+      3, 5, 4  // second Triangle
+  };
+
+  glGenVertexArrays(1, &state.vao);
+  glGenBuffers(1, &state.vbo);
+  glGenBuffers(1, &state.ebo);
+
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s),
+  // and then configure vertex attributes(s).
+  glBindVertexArray(state.vao);
+
+  glBindBuffer(GL_ARRAY_BUFFER, state.vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, state.ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // note that this is allowed, the call to glVertexAttribPointer registered VBO as
+  // the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object
+  // IS stored in the VAO; keep the EBO bound.
+  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+  return state;
 }
